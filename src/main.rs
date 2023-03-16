fbd4132f70f79f2daafedfa2838d5096999961ab
Ruben van Zanten <ruben.vanzanten@student.hu.nl>
2023-03-16 09:27:11 +0100

    yas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+use rppal::i2c::I2c;
+use rppal::pwm::{Channel, Pwm};
+use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use rppal::uart::{Parity, Uart};
+
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_LED: u8 = 23;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
 }
